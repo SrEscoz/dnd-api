c53ec7308927a04901436bdc90b60c34d52be998
Miguel Escoz <kyrovilches@gmail.com>
2024-02-22 19:56:02 +0100

    Commit inicial

---
+++ src/main/java/net/escoz/dndapi/DndApiApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.dndapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/dndapi/DndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.dndapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
